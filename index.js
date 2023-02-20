350fd439bbd1e41e471bc1621a0218fee7076bb6
varelacp <124377343+varelacp@users.noreply.github.com>
2023-02-20 13:22:55 +0000

    done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Iteration 1: Names and Input
-
+const hacker1 = 'Jonh Snow';
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = 'Steve Martin';
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+
+switch ()
 
 
 // Iteration 3: Loops
